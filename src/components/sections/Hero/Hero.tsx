3dee3622552e62018caa1b8e7d45a924a5ed5d53
Jacob Palomo <jacob.palomo248@gmail.com>
2025-02-06 09:48:54 -0600

    Se agregan animaciones al hero section

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Link from 'next/link'
 import MobileVideo from './MobileVideo'
+import { motion } from 'motion/react'
 
 export default function Hero() {
 	return (
@@ -7,18 +10,35 @@ export default function Hero() {
 			<MobileVideo />
 
 			<div className='max-sm: absolute top-[50%] right-[10%] z-10 flex w-1/3 -translate-y-[50%] flex-col gap-10 max-md:right-[inherit] max-md:left-[10%] max-md:w-1/2 max-sm:relative max-sm:right-auto max-sm:left-auto max-sm:w-full max-sm:px-6'>
-				<h2 className='!text-6xl max-md:!text-5xl max-sm:!text-4xl'>
+				<motion.h2
+					initial={{ opacity: 0, filter: 'blur(10px)' }}
+					animate={{ opacity: 1, filter: 'blur(0px)' }}
+					transition={{ duration: 1 }}
+					className='!text-6xl max-md:!text-5xl max-sm:!text-4xl'
+				>
 					Ropa que fluye contigo, porque{' '}
 					<span className='text-[#00aae2]'>la comodidad no tiene bordes</span>.{' '}
 					😉
-				</h2>
+				</motion.h2>
 
-				<Link
-					href='/contacto'
-					className='!w-[max-content] cursor-pointer rounded-lg !bg-[#00aae2] px-6 py-3 font-medium'
+				<motion.div
+					initial={{ scale: 0, originY: 1, originX: 0.2 }}
+					animate={{ scale: 1 }}
+					transition={{
+						duration: 1,
+						delay: 0.5,
+						type: 'spring',
+						stiffness: 200,
+						damping: 16,
+					}}
 				>
-					Ponerse en contacto
-				</Link>
+					<Link
+						href='/contacto'
+						className='!w-[max-content] cursor-pointer rounded-lg !bg-[#00aae2] px-6 py-3 font-medium'
+					>
+						Ponerse en contacto
+					</Link>
+				</motion.div>
 			</div>
 		</section>
 	)
--- src/components/sections/Hero/MobileVideo.tsx
+++ src/components/sections/Hero/MobileVideo.tsx
@@ -7,6 +7,7 @@ import {
 } from '@tabler/icons-react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { motion } from 'motion/react'
 
 export default function MobileVideo() {
 	return (
@@ -16,7 +17,12 @@ export default function MobileVideo() {
 			rel='noopener noreferrer'
 			className='max-sm:flex max-sm:items-center max-sm:justify-center'
 		>
-			<div className='absolute -bottom-[25%] left-[10%] -translate-y-[25%] max-md:-right-[15%] max-md:bottom-[15%] max-md:left-[inherit] max-md:max-h-1/2 max-md:-translate-y-[15%] max-sm:right-auto max-sm:-bottom-[20%] max-sm:left-auto max-sm:-translate-y-[20%]'>
+			<motion.div
+				initial={{ y: '100vh' }}
+				animate={{ y: 0 }}
+				transition={{ type: 'spring', stiffness: 80 }}
+				className='absolute -bottom-[25%] left-[10%] -translate-y-[25%] max-md:-right-[15%] max-md:bottom-[15%] max-md:left-[inherit] max-md:max-h-1/2 max-md:-translate-y-[15%] max-sm:right-auto max-sm:-bottom-[20%] max-sm:left-auto max-sm:-translate-y-[20%]'
+			>
 				<div className='relative w-96 rounded-3xl border-[16px] border-t-[32px] border-white bg-white shadow-2xl before:absolute before:-top-4 before:left-[50%] before:h-2 before:w-2 before:-translate-[50%] before:rounded-sm before:bg-zinc-400 after:absolute after:bottom-2 after:left-[50%] after:h-1 after:w-20 after:-translate-[50%] after:rounded-sm after:bg-white after:opacity-50 after:backdrop-blur-sm'>
 					<div className='absolute top-3 right-3 left-3 flex h-full flex-col justify-between before:absolute before:-top-3 before:-right-3 before:-left-3 before:z-0 before:h-20 before:rounded-t-xl before:bg-gradient-to-b before:from-black/65 before:to-black/0'>
 						<div className='z-10 flex items-center justify-between'>
@@ -39,7 +45,7 @@ export default function MobileVideo() {
 							/>
 						</div>
 
-						<div className='flex items-end justify-between pb-10 before:absolute before:-right-3 before:bottom-3 before:-left-3 before:z-0 before:h-20 before:rounded-lg before:bg-gradient-to-t before:from-black/80 before:to-transparent'>
+						<div className='flex min-h-[1920px] items-end justify-between pb-10 before:absolute before:-right-3 before:bottom-3 before:-left-3 before:z-0 before:h-20 before:rounded-lg before:bg-gradient-to-t before:from-black/80 before:to-transparent'>
 							<div className='z-10 w-4/5'>
 								<h3 className='!mb-2 text-white'>Carrera Viveza 2025</h3>
 								<p className='!text-sm font-medium text-white'>
@@ -103,7 +109,7 @@ export default function MobileVideo() {
 						Tu navegador no es compatible con este video.
 					</video>
 				</div>
-			</div>
+			</motion.div>
 		</Link>
 	)
 }
