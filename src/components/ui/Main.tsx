a6d9efb07af4f4557a4ff079b6849787eabe7674
Jacob Palomo <jacob.palomo248@gmail.com>
2025-02-07 14:40:38 -0600

    Se crea el componente Main para las paginas

--- src/components/ui/Main.tsx
+++ src/components/ui/Main.tsx
@@ -1,10 +1,13 @@
 interface Props {
 	children: React.ReactNode
+	className?: string
 }
 
-const MainContainer: React.FC<Props> = ({ children }) => {
+const MainContainer: React.FC<Props> = ({ children, className }) => {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-center bg-background'>
+		<main
+			className={`flex min-h-screen flex-col items-center justify-center bg-background ${className}`}
+		>
 			{children}
 		</main>
 	)
