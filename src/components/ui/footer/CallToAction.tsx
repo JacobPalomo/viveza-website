abc4724d4a5a4e967682a5e243e60559f2e83955
Jacob Palomo <jacob.palomo248@gmail.com>
2025-02-17 08:18:19 -0600

    Se cambia el fondo del footer y se mejora el aspecto del botón de acción

--- src/components/ui/footer/CallToAction.tsx
+++ src/components/ui/footer/CallToAction.tsx
@@ -2,28 +2,35 @@
 
 import { motion } from 'motion/react'
 import Link from 'next/link'
+import { RiArrowRightUpLine } from 'react-icons/ri'
 
 export function CallToAction() {
 	return (
 		<section>
 			<div className='flex w-full flex-col items-center gap-10'>
 				<div className='flex flex-col items-center'>
-					<h6 className='leading-[1]'>¿Quieres experimientar la diferencia?</h6>
-					<p className='font-clash-grotesk !text-6xl font-semibold max-sm:!text-5xl'>
+					<h6 className='leading-[1] !font-light tracking-wider text-white/85'>
+						¿Quieres experimientar la diferencia?
+					</h6>
+					<p className='font-clash-grotesk !text-6xl !font-normal max-sm:!text-5xl'>
 						¡Comencemos!
 					</p>
 				</div>
 
 				<motion.div
-					whileHover={{ scale: 1.1 }}
-					whileTap={{ scale: 0.8 }}
-					className={`h-12 w-max cursor-pointer justify-center rounded-lg bg-white px-6 text-secondary`}
+					whileHover={{ scale: 1.05 }}
+					whileTap={{ scale: 0.95 }}
+					className={`h-max w-max cursor-pointer justify-center rounded-full bg-white py-2 pr-3 pl-4 text-base text-secondary`}
 				>
 					<Link
 						href='/contacto'
-						className='relative flex h-full w-max items-center justify-center font-[500]'
+						className='relative flex h-full w-max items-center justify-center gap-2 font-[500] uppercase'
 					>
 						Ponerse en contacto
+						<RiArrowRightUpLine
+							size={24}
+							color='var(--color-secondary)'
+						/>
 					</Link>
 				</motion.div>
 			</div>
--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -8,14 +8,14 @@ import VivezaLogo from '@/components/ui/Logos/Viveza'
 
 export function Footer() {
 	return (
-		<footer className='flex min-h-[50vh] w-full flex-col items-center justify-center gap-10 bg-sky-950 pt-20 text-white'>
+		<footer className='flex min-h-[50vh] w-full flex-col items-center justify-center gap-10 bg-sky-950 bg-[url("/api/cdn?url=f_avif,q_auto/bg-footer")] bg-cover pt-40 text-white'>
 			<CallToAction />
 
 			<hr className='h-[1px] w-3/4 bg-white opacity-25' />
 
 			<section className='w-full px-20 max-md:hidden'>
 				<div className='!mb-15 grid w-full grid-cols-2'>
-					<div className='flex w-full flex-col items-start justify-end'>
+					<div className='flex w-max flex-col items-start justify-end'>
 						<Link
 							href='/'
 							aria-label='Viveza Textil'
@@ -24,17 +24,17 @@ export function Footer() {
 							<VivezaLogo size={56} />
 						</Link>
 
-						<div className='flex w-full items-start justify-start gap-10'>
+						<div className='flex w-full items-start justify-center gap-15 opacity-65'>
 							<TiafLogo
-								size={40}
+								size={24}
 								color='white'
 							/>
 							<KinisLogo
-								size={40}
+								size={24}
 								color='white'
 							/>
 							<CasmiLogo
-								size={40}
+								size={24}
 								color='white'
 							/>
 						</div>
