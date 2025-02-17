024e06c4af6fcfcff9ed60428a63fed1da0b7206
Jacob Palomo <jacob.palomo248@gmail.com>
2025-02-17 08:30:04 -0600

    El diseño de titulos estandarizado fue aplicado a las Features

--- src/components/home/features/Feature.tsx
+++ src/components/home/features/Feature.tsx
@@ -1,17 +1,33 @@
+import TitleTag from '@/components/ui/TitleTag'
+
 interface Props {
 	title: React.ReactNode
 	description: string
 	image: React.ReactNode
+	tag: React.ReactNode
 	className?: string
 }
 
-const Feature: React.FC<Props> = ({ title, description, image, className }) => {
+const Feature: React.FC<Props> = ({
+	title,
+	description,
+	image,
+	className,
+	tag,
+}) => {
 	return (
 		<div
 			className={`sticky top-0 flex min-h-screen flex-col justify-center gap-4 rounded-[21px] max-md:min-h-screen ${className}`}
 		>
 			<div className='absolute right-0 bottom-20 left-0 z-10 w-full text-white'>
-				<h3 className='w-full pt-6 pl-6 !text-4xl'>{title}</h3>
+				<TitleTag
+					className='pl-6'
+					textColor='white'
+					divisorColor='white'
+				>
+					{tag}
+				</TitleTag>
+				<h3 className='!mb-6 w-full pl-6'>{title}</h3>
 				<p className='w-full px-6'>{description}</p>
 			</div>
 			<div className='h-full w-full'>
--- src/components/home/features/Features.tsx
+++ src/components/home/features/Features.tsx
@@ -1,22 +1,15 @@
 import Image from 'next/image'
-import Feature from './features/Feature'
+import Feature from './Feature'
 
 export default function Features() {
-	const cookiesAccepted =
-		typeof window !== 'undefined' &&
-		localStorage.getItem('cookiesAccepted') === 'true'
-
 	return (
 		<section className='relative z-20 grid min-h-screen w-full grid-cols-3 grid-rows-4 max-md:grid-cols-1 max-md:grid-rows-3'>
 			<Feature
 				key={'feature-1'}
 				className='bottom-10 col-start-1 col-end-2 row-start-1 row-end-3 max-md:row-end-2'
-				title={
-					<>
-						Calidad <span className='text-primary'>Premium</span>
-					</>
-				}
-				description='Procesos innovadores con tecnología avanzada en textiles.'
+				tag={<>CALIDAD</>}
+				title={<>Perfección en cada detalle</>}
+				description='Procesos innovadores con tecnología avanzada en textiles, garantizando calidad, precisión y durabilidad en cada prenda.'
 				image={
 					<Image
 						src='/api/cdn?url=f_avif,q_auto/calidad'
@@ -26,7 +19,6 @@ export default function Features() {
 						aria-hidden='true'
 						quality={100}
 						loading='lazy'
-						unoptimized={!cookiesAccepted}
 						className='h-screen object-cover brightness-[175%]'
 					/>
 				}
@@ -35,22 +27,18 @@ export default function Features() {
 			<Feature
 				key={'feature-2'}
 				className='col-start-2 col-end-3 row-start-2 row-end-4 max-md:col-start-1 max-md:col-end-2 max-md:row-start-2 max-md:row-end-3'
-				title={
-					<>
-						Diseño <span className='text-primary'>sin Costuras</span>
-					</>
-				}
-				description='Ropa diseñada para la máxima comodidad y frescura.'
+				tag={<>DISEÑO</>}
+				title={<>Confort sin interrupciones</>}
+				description='Ropa diseñada para la máxima comodidad y frescura, con telas ultraligeras y transpirables que se adaptan a tu ritmo.'
 				image={
 					<Image
-						src='/api/cdn?url=f_avif,q_auto/diseno-sin-costuras'
+						src={`/api/cdn?url=/f_avif,q_auto/diseno-sin-costuras`}
 						alt='Imagen que muestra el diseño de nuestra ropa'
 						width={860}
 						height={1147}
 						aria-hidden='true'
 						quality={100}
 						loading='lazy'
-						unoptimized={!cookiesAccepted}
 						className='h-screen object-cover brightness-110'
 					/>
 				}
@@ -59,12 +47,9 @@ export default function Features() {
 			<Feature
 				key={'feature-3'}
 				className='col-start-3 col-end-4 row-start-3 row-end-5 max-md:col-start-1 max-md:col-end-2 max-md:row-start-3 max-md:row-end-4'
-				title={
-					<>
-						Equipo <span className='text-primary'>Dedicado</span>
-					</>
-				}
-				description='Un equipo apasionado que crea productos de alto rendimiento.'
+				tag={<>EQUIPO</>}
+				title={<>Compromiso y excelencia</>}
+				description='Un equipo apasionado que crea productos de alto rendimiento, diseñados para acompañarte en cada desafío.'
 				image={
 					<Image
 						src='/api/cdn?url=f_avif,q_auto/equipo'
@@ -74,7 +59,6 @@ export default function Features() {
 						aria-hidden='true'
 						quality={100}
 						loading='lazy'
-						unoptimized={!cookiesAccepted}
 						className='h-screen object-cover brightness-110'
 					/>
 				}
