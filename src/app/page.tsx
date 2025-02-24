bdb4c5549c11005bca7ff404b6f998067abecd63
Jacob Palomo <jacob.palomo248@gmail.com>
2025-02-24 14:14:13 -0600

    Se elimina importación de componente inexistente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from '@/components/home/hero/Hero'
 import Features from '@/components/home/features/Features'
 import Nosotros from '@/components/home/Nosotros'
 import Marcas from '@/components/home/Marcas'
-import Fact from '@/components/home/Fact'
+// import Fact from '@/components/home/Fact'
 
 export { metadata }
 
