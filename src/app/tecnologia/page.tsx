803adefd88b00c59aac756092a717287ec05316c
Jacob Palomo <jacob.palomo248@gmail.com>
2025-03-19 08:02:31 -0600

    Se implementa la sección PerformanceFabrics

--- src/app/tecnologia/page.tsx
+++ src/app/tecnologia/page.tsx
@@ -3,15 +3,16 @@ import Main from '@/components/ui/Main'
 import Fact from '@/components/tecnologia/Fact'
 import Video from '@/components/tecnologia/Video'
 import Fairs from '@/components/tecnologia/Fairs/Fairs'
-import Section from '@/components/ui/Section'
+import PerformanceFabrics from '@/components/tecnologia/PerformanceFabrics'
 
 export default function Technology() {
 	return (
 		<Main>
 			<Hero />
 			<Fact />
-			<Video />
 			<Fairs />
+			<Video />
+			<PerformanceFabrics />
 		</Main>
 	)
 }
